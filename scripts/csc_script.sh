5d467b74ef88cfcd254142d4007a092b814639e1
Luca Grifo <lg@linux.com>
2017-01-12 20:14:02 +0100

    BIG rework of script All multi csc aks now which specific csc to install (install all is still possible) add many files to XAA (USA csc) Remove redudant XAS csc (all inside XAA)

--- scripts/csc_script.sh
+++ scripts/csc_script.sh
@@ -5,10 +5,17 @@ mount /dev/block/platform/155a0000.ufs/by-name/SYSTEM /system
 mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
 mount /dev/block/platform/155a0000.ufs/by-name/EFS /efs
 
-sed -i -- "s/CSC=//g" /tmp/aroma/csc.prop
 getprop ro.boot.bootloader >> /tmp/variant
-NEW_CSC=`cat /tmp/aroma/csc.prop`
 ACTUAL_CSC=`cat /efs/imei/mps_code.dat`
+
+if grep -q  'CSC=CTI\|CSC=DDE\|CSC=DHR\|CSC=ILO\|CSC=INS\|CSC=SER\|CSC=THL\|CSC=TPA\|CSC=VD2\|CSC=VIA\|CSC=XAA\|CSC=XAC\|CSC=XEF\|CSC=XEO\|CSC=XSG' /tmp/aroma/csc.prop; then
+	echo "cscmulti.prop found"
+	sed -i -- "s/CSC=//g" /tmp/aroma/cscmulti.prop
+	NEW_CSC=`cat /tmp/aroma/cscmulti.prop`
+else
+	sed -i -- "s/CSC=//g" /tmp/aroma/csc.prop
+	NEW_CSC=`cat /tmp/aroma/csc.prop`
+fi
 
 if grep -q G935 /tmp/variant; then
 	sed -i -- "s/G930/G935/g" /system/CSCVersion.txt
@@ -23,14 +30,44 @@ else
 fi
 fi
 
-if [ -f "/tmp/aroma/csc.prop" ]; then
-	echo "csc.prop found"
-	if [ "$NEW_CSC" == "TMB" ]; then
+if grep -q  'CSC=CTI\|CSC=DDE\|CSC=DHR\|CSC=ILO\|CSC=INS\|CSC=SER\|CSC=THL\|CSC=TPA\|CSC=VD2\|CSC=VIA\|CSC=XAA\|CSC=XAC\|CSC=XEF\|CSC=XEO\|CSC=XSG' /tmp/aroma/csc.prop; then
+	echo "apply multi-csc patch"
+	if [ "$NEW_CSC" == "ACG" ]; then
 		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
-		echo "TMB csc detected, flashing ZTO as default"
+		echo "ACG csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "AIO" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "AIO csc detected, flashing ZTO as default"
 	else if [ "$NEW_CSC" == "ATT" ]; then
 		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
 		echo "ATT csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "BST" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "BST csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "GCF" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "GCF csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "LRA" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "LRA csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "SPR" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "SPR csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "TFN" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "TFN csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "TMB" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "TMB csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "TMK" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "TMK csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "USC" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "USC csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "VMU" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "VMU csc detected, flashing ZTO as default"
 	else if [ "$NEW_CSC" == "VZW" ]; then
 		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
 		echo "VZW csc detected, flashing ZTO as default"
@@ -44,6 +81,37 @@ if [ -f "/tmp/aroma/csc.prop" ]; then
 		sed -i -- "s/$ACTUAL_CSC/$NEW_CSC/g" /efs/imei/mps_code.dat
 		echo "flashing selected CSC"
 	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+	fi
+else
+	echo "apply single csc patch"
+	if [ "$NEW_CSC" == "ATT" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "ATT csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "TMB" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "TMB csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "TMK" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "TMK csc detected, flashing ZTO as default"
+	else if [ "$NEW_CSC" == "VZW" ]; then
+		sed -i -- "s/$ACTUAL_CSC/ZTO/g" /efs/imei/mps_code.dat
+		echo "VZW csc detected, flashing ZTO as default"
+	else
+		sed -i -- "s/$ACTUAL_CSC/$NEW_CSC/g" /efs/imei/mps_code.dat
+		echo "flashing selected CSC"
 	fi
 	fi
 	fi
