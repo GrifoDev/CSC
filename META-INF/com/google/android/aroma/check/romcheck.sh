89e3f1c05a2af7dcac04ec7b37ca06555e61ab79
Tkkg1994 <luca@grifodev.ch>
2017-12-11 14:33:30 +0100

    make a check for s7/s8/n8 based roms and through a warning

---
+++ META-INF/com/google/android/aroma/check/romcheck.sh
@@ -0,0 +1,24 @@
+#!/sbin/sh
+# Written by Tkkg1994
+
+mount /dev/block/platform/155a0000.ufs/by-name/SYSTEM /system
+
+cp /system/build.prop /tmp/version
+
+if grep -q "ro.product.first_api_level=23" /tmp/version; then
+	echo "S7 Rom detected"
+else if grep -q "ro.product.first_api_level=24" /tmp/version; then
+	echo "S8 Port detected"
+	sed -i -- "s/s7/s8/g" /tmp/aroma/rom.prop
+else if grep -q "ro.product.first_api_level=25" /tmp/version; then
+	echo "N8 Port detected"
+	sed -i -- "s/s7/n8/g" /tmp/aroma/rom.prop
+else
+	echo "Rom could not be detected successfully"
+	sed -i -- "s/s7/nothing/g" /tmp/aroma/rom.prop
+fi
+fi
+fi
+
+exit 10
+
